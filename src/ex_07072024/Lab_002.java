290edd9a394fb66b8dfd1d30f29a5679c37c0484
TruptiTasre <truptitasare2@gmail.com>
2024-07-08 17:16:56 +0530

    Lab_002 commit

---
+++ src/ex_07072024/Lab_002.java
@@ -0,0 +1,10 @@
+package ex_07072024;
+
+public class Lab_002 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(2+2);
+        System.out.println("Hello World 1");
+        System.out.println("2+2");
+    }
+}
